951de4904b4a89d2ab557b1c324b02fdaf646545
JefMartins <jeffersonmartins.jef@gmail.com>
2023-07-26 14:51:51 -0300

    exercicio finalizado

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] number) {
+        int pyramidSize = 9; //tamanho da pirâmide
+    
+        for(int i = 0; i<pyramidSize; i++){
+            int pyramidLayerNumber = i + 1;
+
+            for(int i2 = 0; i2<pyramidLayerNumber; i2++){
+
+                System.out.print(pyramidLayerNumber);
+
+            };
+            
+            System.out.println();
+        }; 
+    }
+}
